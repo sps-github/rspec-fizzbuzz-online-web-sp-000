96ce56b582d591fa0abb4a73da00bafe4ea7dfb3
Sarah Peters <sarahpieters15@gmail.com>
2020-04-24 17:10:09 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@ def fizzbuzz(n)
   if n % 3 == 0
     puts "Fizz"
     "Fizz"
-  elsif (n % 5) == 0
+  elsif n % 5 == 0
     puts "Buzz"
     "Buzz"
-  elsif (n % 3) && (n % 5) == 0
+  elsif n % 3 && n % 5 == 0
     puts "FizzBuzz"
     "FizzBuzz"
   else
